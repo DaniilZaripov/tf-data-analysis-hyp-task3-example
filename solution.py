41da6d785d0cf9e1ab1c5171726b0b3ce9211a09
DaniilZaripov <129584571+DaniilZaripov@users.noreply.github.com>
2023-04-12 00:49:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,10 +4,7 @@ from scipy.stats import ttest_ind
 
 chat_id = 931254925 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+def solution(x: np.array, y: np.array) -> bool:
      _, p_value = ttest_ind(x, y, permutations=5000, alternative='greater')
     alpha = 0.1
     return p_value < alpha
