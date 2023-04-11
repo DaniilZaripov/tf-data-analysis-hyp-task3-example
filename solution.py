ba4cfd1f2e459a6b6f411b54f7994c2335e3db3a
DaniilZaripov <129584571+DaniilZaripov@users.noreply.github.com>
2023-04-12 01:46:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ from scipy.stats import ttest_ind
 chat_id = 931254925 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-     _, p_value = ttest_ind(x, y, permutations=5000, alternative='greater')
+     _, p_value = ttest_ind(x, y, permutations=5000, equal_var=False, alternative='less')
     alpha = 0.1
     return p_value < alpha
